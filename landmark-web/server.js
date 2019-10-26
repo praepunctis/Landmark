9000d70ca4e1ce9ff192fe02c84ce543547c8054
Jason May <jwmay@ucsc.edu>
2019-10-26 15:56:15 -0700

    updated file pathing

--- landmark-web/server.js
+++ landmark-web/server.js
@@ -7,4 +7,16 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/src/css/main.css',function(req,res){
+  res.sendFile(path.join(__dirname + '/src/css/main.css')); 
+});
+
+app.get('/src/js/Mapbox.js',function(req,res){
+  res.sendFile(path.join(__dirname + '/src/js/Mapbox.js')); 
+});
+
+// app.get('/src/js/Cockroach.js',function(req,res){
+//   res.sendFile(path.join(__dirname + '/src/js/Cockroach.js')); 
+// });
+
 app.listen(8080);
